a5f4f42411663444538d2a77f86b221765606040
ctpatey <ctpatey@gmail.com>
2017-07-22 23:09:46 -0400

    upload first book functionality

---
+++ app/controllers/book_one_controller.rb
@@ -0,0 +1,4 @@
+class BookOneController < ApplicationController
+	def home
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  get 'page/home'
+  root 'page#home'
+
+  get 'book_one/home'
+  get 'book_one/beef_page_two'
+  get 'book_one/bread_page_three'
+  get 'book_one/cafeteria_page_four'
+  get 'book_one/chicken_page_five'
+  get 'book_one/fish_page_six'
+  get 'book_one/pasta_page_seven'
+  get 'book_one/pork_page_eight'
+  get 'book_one/potato_page_nine'
+  get 'book_one/rice_page_ten'
+  get 'book_one/salad_page_eleven'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
